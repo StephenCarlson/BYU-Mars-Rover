ac548758832cb7f7628c973e07e1f677761038a1
Stephen Carlson <stephen_carlson@byu.edu>
2014-10-21 21:52:54 -0600

    RLv2 Modified, GUI Features Added
    Signed-off-by: Stephen Carlson <stephen_carlson@byu.edu>

--- joystick.py
+++ joystick.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import socket
 
 # UDP_IP = "192.168.1.143"
@@ -28,6 +29,16 @@ sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
 BLACK    = (   0,   0,   0)
 WHITE    = ( 255, 255, 255)
 
+allowMouseIntegral = False
+# gimbalPan = 0
+# gimbalTilt = 0
+gimbal = [0,0]
+
+# driveFwd = 0;
+# driveTurn = 0;
+drive = [0,0]
+
+
 # This is a simple class that will help us printScr to the screen
 # It has nothing to do with the joysticks, just outputing the
 # information.
@@ -56,7 +67,7 @@ class TextprintScr:
 pygame.init()
 
 # Set the width and height of the screen [width,height]
-size = [500, 700]
+size = [500, 300]
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("BYU Rover Control Panel")
@@ -85,13 +96,84 @@ while done==False:
 			print("Joystick button pressed.")
 		if event.type == pygame.JOYBUTTONUP:
 			print("Joystick button released.")
+		if event.type == MOUSEBUTTONDOWN:
+			#print event.button
+			if event.button==1: 
+				print "LMB"
+				allowMouseIntegral = not allowMouseIntegral
+				pygame.event.set_grab(allowMouseIntegral)
+				pygame.mouse.set_visible(not allowMouseIntegral)
+				if(not allowMouseIntegral): pygame.mouse.set_pos((348+int(gimbal[0]/20), 148+int(gimbal[1]/20)))
+			if event.button==3 and allowMouseIntegral:
+				print "RMB"
+				# gimbalPan= 0
+				# gimbalTilt= 0
+				gimbal = [0,0]
+			if event.button==2:
+				print "MMB"
+				
+		if event.type == MOUSEBUTTONUP:
+			#print event.button
+			if event.button==1: print "lmb"			
+		if event.type == KEYDOWN:
+			#print event.key
+			if event.key==K_w: 
+				print 'W'
+				#driveFwd = 1000
+				#driveFwd -= int(abs(driveFwd*.1))+1
+				# print driveFwd
+			if event.key==K_a: 
+				print 'A'
+				#driveTurn = -1000
+				#driveFwd -= int(abs(driveFwd*.1))+1
+			if event.key==K_s: 
+				print 'S'
+				#driveFwd = -1000
+				#driveFwd -= int(abs(driveFwd*.1))+1
+			if event.key==K_d: 
+				print 'D'
+				#driveTurn = 1000
+				#driveFwd -= int(abs(driveFwd*.1))+1
+		if event.type == KEYUP:
+			#print event.key
+			if event.key==K_w: 
+				print 'w'
+				#driveFwd = 0
+			if event.key==K_a: 
+				print 'a'
+				#driveTurn = 0
+			if event.key==K_s: 
+				print 's'
+				#driveFwd = 0
+			if event.key==K_d: 
+				print 'd'
+				#driveTurn = 0
+				
 			
-
 	# DRAWING STEP
 	# First, clear the screen to white. Don't put other drawing commands
 	# above this, or they will be erased with this command.
 	screen.fill(WHITE)
 	textprintScr.reset()
+	
+	# --- Capture Mouse and set Gimbal
+	#print pygame.mouse.get_pos()
+	#mouseX,mouseY = pygame.mouse.get_pos()
+	mouseX,mouseY = pygame.mouse.get_rel()
+	if allowMouseIntegral:
+		# gimbalPan += mouseX
+		# gimbalTilt += mouseY
+		gimbal[0] += mouseX
+		gimbal[1] += mouseY
+	
+	#print mouseX,mouseY
+	#textprintScr.printScr(screen, "Gimbal: {} {}".format(gimbalPan,gimbalTilt))
+	textprintScr.printScr(screen, "Gimbal: {} {}".format(gimbal[0],gimbal[1]))
+	
+	# --- Aquire Key States and set Drive accordingly
+	keystate = pygame.key.get_pressed()
+	drive = [(keystate[K_d]-keystate[K_a])*1000,(keystate[K_w]-keystate[K_s])*1000]
+	
 
 	# Get count of joysticks
 	joystick_count = pygame.joystick.get_count()
@@ -146,8 +228,51 @@ while done==False:
 		
 		payload = repr(joystick.get_axis(0))
 		sock.sendto(payload, (UDP_IP, UDP_PORT))
-		
-
+	
+	# --- Over-ride the Mouse/Keyboard setpoints if joystick is present
+	if(pygame.joystick.get_count()==1):
+		joystick = pygame.joystick.Joystick(i)
+		joystick.init()
+		if(joystick.get_numaxes() >=2 ):
+			drive[0] = int(joystick.get_axis(0)*1000)
+			drive[1] = int(joystick.get_axis(1)*1000)
+		if(joystick.get_numaxes() >=4 ):
+			gimbal[0] = int(joystick.get_axis(2)*1000)
+			gimbal[1] = int(joystick.get_axis(3)*1000)
+	
+	
+	# --- Constrain all values to +/- 1000
+	#if gimbalPan in range(-1000,1000)
+	#if gimbalTilt in range(-1000,1000)
+	
+	# if gimbalPan > 1000: gimbalPan= 1000
+	# if gimbalPan < -1000: gimbalPan= -1000
+	# if gimbalTilt > 1000: gimbalTilt= 1000
+	# if gimbalTilt < -1000: gimbalTilt= -1000
+	
+	for i in range(0,len(gimbal)):
+		if gimbal[i] > 1000: gimbal[i]= 1000
+		if gimbal[i] < -1000: gimbal[i]= -1000
+	
+	# if driveFwd > 1000: driveFwd= 1000
+	# if driveFwd < -1000: driveFwd= -1000
+	# if driveTurn > 1000: driveTurn= 1000
+	# if driveTurn < -1000: driveTurn= -1000
+	for i in range(0,len(drive)):
+		if drive[i] > 1000: drive[i] = 1000
+		if drive[i] < -1000: drive[i] = -1000
+	
+	
+	# --- Draw values
+	pygame.draw.rect(screen, (255, 128, 0), (98, 98, 104, 104),2)
+	pygame.draw.rect(screen, (0, 128, 255), (148+int(drive[0]/20), 148+int(-drive[1]/20), 4, 4))
+	pygame.draw.rect(screen, (0, 0, 0), (298, 98, 104, 104),2)
+	pygame.draw.rect(screen, (0, 0, 0), (348+int(gimbal[0]/20), 148+int(gimbal[1]/20), 4, 4))
+	
+	# --- Assert UDP packet values
+	
+	
+	
 	
 	# ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
 	
